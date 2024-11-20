58b52eea7bc3ec16c46121eb07547aefd241e4ec
Rohit-Palavanchu <rohitp.vja@gmail.com>
2024-11-20 10:39:19 +0530

    edited the apis

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -9,7 +9,7 @@ function UserList() {
 
   const fetchUsers = async () => {
     try {
-      const response = await axios.get('http://localhost:3000/users');
+      const response = await axios.get('https://ajackusbackend.onrender.com/users');
       setUsers(response.data);
     } catch (error) {
       alert('Error fetching users');
@@ -18,7 +18,7 @@ function UserList() {
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`http://localhost:3000/users/${id}`);
+      await axios.delete(`https://ajackusbackend.onrender.com/${id}`);
       setUsers(users.filter((user) => user.id !== id));
     } catch (error) {
       alert('Error deleting user');
@@ -28,9 +28,9 @@ function UserList() {
   const handleSave = async (user) => {
     try {
       if (user.id) {
-        await axios.put(`http://localhost:3000/users/${user.id}`, user);
+        await axios.put(`https://ajackusbackend.onrender.com/${user.id}`, user);
       } else {
-        const response = await axios.post('http://localhost:3000/users', user);
+        const response = await axios.post('https://ajackusbackend.onrender.com/users', user);
         setUsers([...users, response.data]);
       }
       setEditingUser(null);
